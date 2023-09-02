68bef31fd850cf4e4f8d379978a1881ba6044733
RentlyNandha <nandhakumar.m@rently.com>
2023-09-02 06:26:47 +0530

    #test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,39 @@ class ProductsController < ApplicationController
     @products = @category.products.includes(:variants).order(:title)
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to category_products_path(@product.category_id)
+    else
+      flash[:error] = "error occured"
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @variants = @product.variants
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to category_products_path(@product.category_id)
+  end
+  def add_new_category
+    @product = Product.find(params[:id])
+    @category = @product.create_category(category_params)
+
+    if @category.present?
+      redirect_to category_products_path(@category)
+    else
+      flash[:error] = "error occured"
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :category_id)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,36 @@ class Category < ApplicationRecord
   has_many :product_categories
   has_many :products, through: :product_categories
 
+  def self.with_products
+    joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
+  end
+
+  def self.with_products_count
+    joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
+  end
+
+  def self.with_products_count_and_variants_count
+    joins(:products).select('categories.*, count(products.id) as products_count, sum(products.variants_count) as variants_count').group('categories.id').order(:title)
+  end
+
+  def self.with_products_count_and_variants_count_and_min_price
+    joins(:products).select('categories.*, count(products.id) as products_count, sum(products.variants_count) as variants_count, min(products.min_price) as min_price').group('categories.id').order(:title)
+  end
+
+  def self.with_products_count_and_variants_count_and_min_price_and_max_price
+    joins(:products).select('categories.*, count(products.id) as products_count, sum(products.variants_count) as variants_count, min(products.min_price) as min_price, max(products.max_price) as max_price').group('categories.id').order(:title)
+  end
+
+  def self.with_products_count_and_variants_count_and_min_price_and_max_price_and_avg_price
+    joins(:products).select('categories.*, count(products.id) as products_count, sum(products.variants_count) as variants_count, min(products.min_price) as min_price, max(products.max_price) as max_price, avg(products.avg_price) as avg_price').group('categories.id').order(:title)
+  end
+
+  def self.with_products_count_and_variants_count_and_min_price_and_max_price_and_avg_price_and_avg_weight
+    joins(:products).select('categories.*, count(products.id) as products_count, sum(products.variants_count) as variants_count, min(products.min_price) as min_price, max(products.max_price) as max_price, avg(products.avg_price) as avg_price, avg(products.avg_weight) as avg_weight').group('categories.id').order(:title)
+  end
+
+  def remove_product(product_id)
+    product = self.products.find(product_id)
+    product.destroy
+  end
 end
