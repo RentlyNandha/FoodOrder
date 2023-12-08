76ea5384c5db5635d23d10332e48463b633ebf9f
RentlyNandha <nandhakumar.m@rently.com>
2023-12-08 15:55:05 +0530

    #added create functionality for deliverables

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -3,4 +3,19 @@ class DeliveriesController < ApplicationController
   def index
     @deliveries = Delivery.includes(:delivery_man)
   end
+
+  def create
+    @delivery = Delivery.new(delivery_params)
+    if @delivery.save
+      redirect_to deliveries_path
+    else
+      flash[:error] = "error occured"
+      render :new
+    end
+  end
+
+  private
+  def delivery_params
+    params.permit(:feedback, :status)
+  end
 end
